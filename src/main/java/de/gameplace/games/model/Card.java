381444a3a42ba05f1c464c73ec952dbb1e35a54b
Jules <jul.sandrock@gmail.com>
2020-04-08 23:19:29 +0200

    Rename CardColor enum

--- src/main/java/de/gameplace/games/model/Card.java
+++ src/main/java/de/gameplace/games/model/Card.java
@@ -5,9 +5,9 @@ import lombok.Data;
 @Data
 public class Card {
 
-    Color color;
+    CardColor cardColor;
     int value; // 1 - 13: color card values, 0: narr, 14: wizard
 
-    public enum Color {Red, Blue, Green, Yello, White};
+    public enum CardColor {RED, BLUE, GREEN, YELLOW, WHITE};
 
 }
