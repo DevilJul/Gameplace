b9e201f6fc192efd124d52b2f3e4ceda6f4bd830
Jules <jul.sandrock@gmail.com>
2020-04-08 12:28:53 +0200

    Add GET methods for all players

--- src/main/java/de/gameplace/games/rest/PlayerController.java
+++ src/main/java/de/gameplace/games/rest/PlayerController.java
@@ -1,15 +1,17 @@
 package de.gameplace.games.rest;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import de.gameplace.games.exceptions.PlayerNameException;
+import de.gameplace.games.exceptions.GameException;
 import de.gameplace.games.model.PlayerOut;
 import de.gameplace.games.service.GameService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
@@ -19,12 +21,22 @@ public class PlayerController {
     private GameService gameService;
 
     @GetMapping("/player")
-    public PlayerOut getPlayer(@RequestParam(value="name", defaultValue="") String name) throws PlayerNameException {
+    public PlayerOut getPlayer(@RequestParam(value="name", defaultValue="") String name) throws GameException {
         return gameService.getPlayerOut(name);
     }
 
+    @GetMapping("/players")
+    public List<PlayerOut> getPlayers() throws GameException {
+        return gameService.getPlayersOut();
+    }
+
+    @GetMapping("/addPlayer")
+    public PlayerOut addPlayer(@RequestParam(value="name", defaultValue="") String name) throws GameException {
+        return gameService.addPlayer(name);
+    }
+
     @PostMapping("addPlayer")
-    public PlayerOut addPlayer(@RequestBody PlayerOut playerFromUser) throws PlayerNameException {       
+    public PlayerOut addPlayer(@RequestBody PlayerOut playerFromUser) throws GameException {       
         return gameService.addPlayer(playerFromUser);
     }
     
