3f79d05ea340c3fdcee025e208e44b654beacd93
Jules <jul.sandrock@gmail.com>
2020-04-08 23:22:07 +0200

    Start implementing GamestatePlay

--- src/main/java/de/gameplace/games/service/GamestatePlayService.java
+++ src/main/java/de/gameplace/games/service/GamestatePlayService.java
@@ -1,5 +1,29 @@
 package de.gameplace.games.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import de.gameplace.games.exceptions.GameException;
+import de.gameplace.games.model.Game;
+import de.gameplace.games.model.Player;
+
+@Service
 public class GamestatePlayService {
+
+    @Autowired
+    Game game;
+
+    @Autowired
+    GameService gameService;
+
+    @Autowired
+    GamePlayService gamePlayService;
+
+    public void playCard(Player player, String cardString) throws GameException {
+        gameService.checkPlayerIsAllowedToPlay(player);
+        
+        
+    }
+
 
 }
