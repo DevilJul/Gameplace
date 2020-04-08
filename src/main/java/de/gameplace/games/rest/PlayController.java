1302fc1801f68325a446b4e914fd967b8ba85f6c
Jules <jul.sandrock@gmail.com>
2020-04-08 23:20:22 +0200

    PlayController must have annotation RestController

--- src/main/java/de/gameplace/games/rest/PlayController.java
+++ src/main/java/de/gameplace/games/rest/PlayController.java
@@ -3,10 +3,12 @@ package de.gameplace.games.rest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import de.gameplace.games.exceptions.GameException;
 import de.gameplace.games.service.GamePlayService;
 
+@RestController
 public class PlayController {
 
     @Autowired
