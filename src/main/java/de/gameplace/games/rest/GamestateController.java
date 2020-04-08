52bcd7c67f503045909125f75f1762652fea1e2a
Jules <jul.sandrock@gmail.com>
2020-04-08 19:32:20 +0200

    helper class to temporarily show game state

---
+++ src/main/java/de/gameplace/games/rest/GamestateController.java
@@ -0,0 +1,28 @@
+package de.gameplace.games.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import de.gameplace.games.model.Game;
+import de.gameplace.games.model.Player;
+
+@RestController
+public class GamestateController {
+
+    @Autowired
+    Game game;
+
+    @GetMapping("/game")
+    public Game getGame() {
+        return game;
+    }
+
+    @GetMapping("/playersintern")
+    public List<Player> getPlayersIntern() {
+        return game.getPlayers();
+    }
+
+}
