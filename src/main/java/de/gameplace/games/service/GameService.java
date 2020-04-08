c8d1b16e4a237235f6c8430cc88d4f89b46ef5b9
Jules <jul.sandrock@gmail.com>
2020-04-08 19:35:44 +0200

    Service to provide information about game

--- src/main/java/de/gameplace/games/service/GameService.java
+++ src/main/java/de/gameplace/games/service/GameService.java
@@ -1,20 +1,9 @@
 package de.gameplace.games.service;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import de.gameplace.games.configuration.InitPlayer;
-import de.gameplace.games.exceptions.GameException;
-import de.gameplace.games.exceptions.NoNewPlayerAllowedException;
-import de.gameplace.games.exceptions.PlayerNameException;
 import de.gameplace.games.model.Game;
-import de.gameplace.games.model.Player;
-import de.gameplace.games.model.PlayerOut;
 
 @Service
 public class GameService {
@@ -22,46 +11,27 @@ public class GameService {
     @Autowired
     Game game;
 
-    //@Value("$(wizard.maxPlayers)") TODO
-    int maxPlayers = 6;
-
-    public PlayerOut getPlayerOut(final String name) throws GameException {
-        return convertPlayerToOut(getPlayerByName(name));
+    public boolean allPlayersOkay() {
+        return game.getPlayers().stream().allMatch(p -> p.isCurrentOkayState());
     }
 
-    public List<PlayerOut> getPlayersOut() throws GameException {
-        return game.getPlayers().stream().map(p -> InitPlayer.playerOut(p)).collect(Collectors.toList());
+    public boolean isCurrentActionPlayerLast() {
+        return calcIncrementPlayerIndex(game.getCurrentActionPlayerIndex()) == game.getCurrentStartPlayerIndex();
     }
 
-    public PlayerOut addPlayer(final String name) throws GameException {
+    public void incrementCurrentActionPlayer() {
+        game.setCurrentActionPlayerIndex(calcIncrementPlayerIndex(game.getCurrentActionPlayerIndex()));
+    }
 
-        // TODO: If game is running, new players can't be added
+    public void incrementCurrentStartPlayer() {
+        game.setCurrentStartPlayerIndex(calcIncrementPlayerIndex(game.getCurrentStartPlayerIndex()));
+    }
 
-        if (game.getPlayers().size() >= maxPlayers) {
-            throw new NoNewPlayerAllowedException();
+    private int calcIncrementPlayerIndex(int currentIndex) {
+        currentIndex++;
+        if (currentIndex == game.getPlayers().size()) {
+            currentIndex = 0;
         }
-
-        if (getPlayerByName(name).isPresent()) {
-            throw new PlayerNameException();
-        }
-
-        Player newPlayer = InitPlayer.player(name);
-        game.getPlayers().add(newPlayer);
-
-        return InitPlayer.playerOut(newPlayer);
+        return currentIndex;
     }
-
-    public PlayerOut addPlayer(final PlayerOut playerFromUser) throws GameException {
-        // TODO create player if allowed
-        return getPlayerOut(playerFromUser.getName());
-    }
-
-    private PlayerOut convertPlayerToOut(final Optional<Player> playerIntern) {
-        return playerIntern.map(p -> InitPlayer.playerOut(p)).orElse(null);
-    }
-
-    private Optional<Player> getPlayerByName(final String name) {
-        return game.getPlayers().stream().filter(p -> p.getName().equals(name)).findAny();
-    }
-
 }
