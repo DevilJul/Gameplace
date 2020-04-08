331280ec5c9a597121f39572ff3dfcedd4ba6754
Jules <jul.sandrock@gmail.com>
2020-04-08 12:27:53 +0200

    Add static constructors for Player and PlayerOut

---
+++ src/main/java/de/gameplace/games/configuration/InitPlayer.java
@@ -0,0 +1,39 @@
+package de.gameplace.games.configuration;
+
+import java.util.ArrayList;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import de.gameplace.games.model.Player;
+import de.gameplace.games.model.PlayerOut;
+
+@Configuration
+public abstract class InitPlayer {
+
+    //@Bean
+    public static Player player(String name) {
+        Player player = new Player();
+        player.setName(name);
+        player.setBets(new ArrayList<>());
+        player.setTricks(new ArrayList<>());
+        player.setPoints(new ArrayList<>());
+        player.setCurrentCards(new ArrayList<>());
+        player.setPointsTotal(0);
+
+        return player;
+    }
+
+    //@Bean
+    public static PlayerOut playerOut(Player playerIntern) {
+        PlayerOut playerOut = new PlayerOut();
+        playerOut.setName(playerIntern.getName());
+        playerOut.setCards(playerIntern.getCurrentCards());
+        playerOut.setPoints(playerIntern.getPointsTotal());
+        playerOut.setBets(playerIntern.getBets() == null || playerIntern.getBets().isEmpty() ? 0
+                : playerIntern.getBets().get(playerIntern.getBets().size() - 1));
+
+        return playerOut;
+    }
+
+}
