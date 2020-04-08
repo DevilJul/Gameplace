2e25b0f32e7b16af66076f2871430886d93f268e
Jules <jul.sandrock@gmail.com>
2020-04-08 19:34:12 +0200

    Seperate play controller and player controller

---
+++ src/main/java/de/gameplace/games/rest/PlayController.java
@@ -0,0 +1,35 @@
+package de.gameplace.games.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import de.gameplace.games.exceptions.GameException;
+import de.gameplace.games.service.GamePlayService;
+
+public class PlayController {
+
+    @Autowired
+    private GamePlayService gamePlayService;
+
+    @GetMapping("/playCard")
+    public void playCard(@RequestParam(value="player", defaultValue = "") String playerName,
+                         @RequestParam(value="card", defaultValue = "") String cardString)
+                         throws GameException {
+        gamePlayService.playCard(playerName, cardString);
+    }
+
+    @GetMapping("/playBet")
+    public void playBet(@RequestParam(value="player", defaultValue = "") String playerName,
+                         @RequestParam(value="bet", defaultValue = "") String bet)
+                         throws GameException {
+        gamePlayService.playBet(playerName, bet);
+    }
+
+    @GetMapping("/playOkay")
+    public void playOkay(@RequestParam(value="player", defaultValue = "") String playerName)
+                         throws GameException {
+        gamePlayService.playOkay(playerName);
+    }
+
+}
--- src/main/java/de/gameplace/games/rest/PlayerController.java
+++ src/main/java/de/gameplace/games/rest/PlayerController.java
@@ -11,33 +11,37 @@ import org.springframework.web.bind.annotation.RestController;
 
 import de.gameplace.games.exceptions.GameException;
 import de.gameplace.games.model.PlayerOut;
-import de.gameplace.games.service.GameService;
+import de.gameplace.games.service.GamestateInitService;
+import de.gameplace.games.service.PlayerService;
 
 
 @RestController
 public class PlayerController {
 
     @Autowired
-    private GameService gameService;
+    private PlayerService playerService;
+
+    @Autowired
+    private GamestateInitService gameInitService;
 
     @GetMapping("/player")
     public PlayerOut getPlayer(@RequestParam(value="name", defaultValue="") String name) throws GameException {
-        return gameService.getPlayerOut(name);
+        return playerService.getPlayerOut(name);
     }
 
     @GetMapping("/players")
     public List<PlayerOut> getPlayers() throws GameException {
-        return gameService.getPlayersOut();
+        return playerService.getPlayersOut();
     }
 
     @GetMapping("/addPlayer")
     public PlayerOut addPlayer(@RequestParam(value="name", defaultValue="") String name) throws GameException {
-        return gameService.addPlayer(name);
+        return gameInitService.addPlayer(name);
     }
 
     @PostMapping("addPlayer")
     public PlayerOut addPlayer(@RequestBody PlayerOut playerFromUser) throws GameException {       
-        return gameService.addPlayer(playerFromUser);
+        return playerService.addPlayer(playerFromUser);
     }
     
 }
