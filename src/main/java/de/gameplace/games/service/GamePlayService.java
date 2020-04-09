b2090af0e568590164470a900f13e25e0879446b
Jules <jul.sandrock@gmail.com>
2020-04-09 16:12:28 +0200

    improve gamestate bet

--- src/main/java/de/gameplace/games/service/GamePlayService.java
+++ src/main/java/de/gameplace/games/service/GamePlayService.java
@@ -15,7 +15,10 @@ import de.gameplace.games.model.Card;
 import de.gameplace.games.model.Game;
 import de.gameplace.games.model.GameRound;
 import de.gameplace.games.model.Game.GamestateEnum;
+import de.gameplace.games.tools.CardHelper;
+import de.gameplace.games.tools.ListHelper;
 import de.gameplace.games.model.Player;
+import de.gameplace.games.model.Card.CardColor;
 
 @Service
 public class GamePlayService {
@@ -33,31 +36,46 @@ public class GamePlayService {
     GamestateBetService gamestateBetService;
 
     @Autowired
+    GamestateWaitService gamestateWaitService;
+
+    @Autowired
     GamestatePlayService gamestatePlayService;
 
     @Autowired
     GameService gameService;
 
-    public void playCard(String playerName, String cardString) throws GameException {
+    public Player playCard(String playerName, String cardColorString, String cardValueString) throws GameException {
         Player player = resolvePlayerAndCheckGameState(GamestateEnum.PLAY, playerName);
-        gamestatePlayService.playCard(player, cardString);
+        gamestatePlayService.playCard(player, cardColorString, cardValueString);
+
+        if (isLastCard()) {
+            endRound();
+        }
+        return player;
     }
 
-    public void playBet(String playerName, String betString) throws GameException {
+    private boolean isLastCard() {
+        return game.getPlayers().stream().map(p -> p.getCurrentCards()).allMatch(l -> l.isEmpty());
+    }
+
+    public Player playBet(String playerName, String betString) throws GameException {
         Player player = resolvePlayerAndCheckGameState(GamestateEnum.BET, playerName);
         gamestateBetService.playBet(player, betString);
+        return player;
     }
 
-    public void playOkay(String playerName) throws GameException {
+    public Player playOkay(String playerName) throws GameException {
         Player player = playerService.getPlayerByName(playerName);
         switch (game.getGamestate()) {
             case INIT:
             gamestateInitService.setPlayerOkay(player);
                 break;
-            case BET:
+            case WAIT:
+            gamestateWaitService.setPlayerOkay(player);
             default:
                 break;
         }
+        return player;
     }
 
     private Player resolvePlayerAndCheckGameState(GamestateEnum expectedGamestate, String playerName) throws GameException {
@@ -71,8 +89,13 @@ public class GamePlayService {
     public void startGame() {
         game.setCurrentStartPlayerIndex(0);
         game.setCurrentRound(1);
+        setNumberOfRounds();
 
         startRound();
+    }
+
+    private void setNumberOfRounds() {
+        game.setNumberOfRounds(60 / game.getPlayers().size());
     }
 
     public void startRound() {
@@ -85,9 +108,27 @@ public class GamePlayService {
     }
 
     public void endRound() {
+        calculatePoints();
         game.setCurrentRound(game.getCurrentRound() + 1);
         gameService.incrementCurrentStartPlayer();
-        // TODO more...
+        proceedToGameStateWait();
+    }
+
+    private void calculatePoints() {
+
+        for (Player player : game.getPlayers()) {
+            int bet = ListHelper.getLastValue(player.getBets());
+            int tricks = ListHelper.getLastValue(player.getTricks());
+            int points = 0;
+            if (bet == tricks) {
+                points = 20 + tricks * 10;
+            } else {
+                points = 0 - (Math.abs(bet - tricks) * 10);
+            }
+            player.getPoints().add(points);
+            player.setPointsTotal(player.getPointsTotal() + points);
+        }
+
     }
 
     public void proceedToGamestatePlay() {
@@ -100,31 +141,44 @@ public class GamePlayService {
         game.getPlayers().stream().forEach(p -> {
             p.setCurrentOkayState(false);
             p.getBets().add(0);
+            p.getTricks().add(0);
         });
         dealCards();
     }
 
+    public void proceedToGameStateWait() {
+        game.setGamestate(GamestateEnum.WAIT);
+    }
+
     public void proceedToGamestateEnd() {
-        
+        game.setGamestate(GamestateEnum.END);
     }
 
     private void dealCards() {
         int numberOfCards = game.getCurrentRound() * game.getPlayers().size();
         List<Integer> allCardIndices = IntStream.range(0, game.getCards().size()).boxed().collect(Collectors.toList());
-        List<Integer> dealedCardsIndices = new ArrayList<Integer>(numberOfCards);
-
-        Random rand = new Random();
-        for (int i = 0; i < numberOfCards; i++) {
-            dealedCardsIndices.add(allCardIndices.remove(rand.nextInt(allCardIndices.size())));
-        }
 
         game.getPlayers().forEach(p -> p.setCurrentCards(new ArrayList<Card>(game.getCurrentRound())));
 
-        for (int iterCards = 0; iterCards < dealedCardsIndices.size(); iterCards++) {
-            game.getPlayers().get(iterCards % game.getPlayers().size()).getCurrentCards().add(game.getCards().get(dealedCardsIndices.get(iterCards)));
+        Random rand = new Random();
+        for (int iterCards = 0; iterCards < numberOfCards; iterCards++) {
+            int cardIndex = allCardIndices.remove(rand.nextInt(allCardIndices.size()));
+            game.getPlayers().get(iterCards % game.getPlayers().size()).getCurrentCards().add(game.getCards().get(cardIndex));
         }
 
-        game.getGameRound().setTrumpCard(game.getCards().get(allCardIndices.remove(rand.nextInt(allCardIndices.size()))));
+        setTrumpCard(allCardIndices.get(rand.nextInt(allCardIndices.size())));
+    }
+
+    private void setTrumpCard(int cardIndex) {
+        Card trumpCard = game.getCards().get(cardIndex);
+        if (CardHelper.isWizard(trumpCard)) {
+            trumpCard.setCardColor(selectRandomCardColor());
+        }
+    }
+
+    private CardColor selectRandomCardColor() {
+        Random rand = new Random();
+        return CardColor.values()[rand.nextInt(CardColor.values().length - 1)];
     }
 
 }
