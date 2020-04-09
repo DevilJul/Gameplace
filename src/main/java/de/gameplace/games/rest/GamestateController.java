495d4420d10e16037efee4902a8092486f4279bd
Jules <jul.sandrock@gmail.com>
2020-04-09 16:10:56 +0200

    add helpers for tests

--- src/main/java/de/gameplace/games/rest/GamestateController.java
+++ src/main/java/de/gameplace/games/rest/GamestateController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import de.gameplace.games.model.Game;
+import de.gameplace.games.model.GameRound;
 import de.gameplace.games.model.Player;
 
 @RestController
@@ -18,6 +19,11 @@ public class GamestateController {
     @GetMapping("/game")
     public Game getGame() {
         return game;
+    }
+
+    @GetMapping("/gameround")
+    public GameRound getGameRound() {
+        return game.getGameRound();
     }
 
     @GetMapping("/playersintern")
