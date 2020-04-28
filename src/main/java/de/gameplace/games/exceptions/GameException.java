3f1d004e474aa8203197a5a88fd9fb851bb87347
Jules <jul.sandrock@gmail.com>
2020-04-28 14:48:52 +0200

    broaden Exceptions

--- src/main/java/de/gameplace/games/exceptions/GameException.java
+++ src/main/java/de/gameplace/games/exceptions/GameException.java
@@ -2,4 +2,11 @@ package de.gameplace.games.exceptions;
 
 public class GameException extends Exception {
 
+    public GameException() {
+        super();
+    }
+
+    public GameException (String message) {
+        super(message);
+    }
 }
--- src/main/java/de/gameplace/games/exceptions/PlayerNameException.java
+++ src/main/java/de/gameplace/games/exceptions/PlayerNameException.java
@@ -2,4 +2,12 @@ package de.gameplace.games.exceptions;
 
 public class PlayerNameException extends GameException {
 
+    public PlayerNameException() {
+        super();
+    }
+
+	public PlayerNameException(String message) {
+        super(message);
+	}
+
 }
