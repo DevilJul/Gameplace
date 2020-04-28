7787020e1203287f4473d883b37e661d14be025d
Jules <jul.sandrock@gmail.com>
2020-04-28 18:15:39 +0200

    Add Bot registration to main

--- src/main/java/de/gameplace/games/GamesApplication.java
+++ src/main/java/de/gameplace/games/GamesApplication.java
@@ -2,11 +2,14 @@ package de.gameplace.games;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.ApiContextInitializer;
 
 @SpringBootApplication
 public class GamesApplication {
 
 	public static void main(String[] args) {
+		ApiContextInitializer.init();
+
 		SpringApplication.run(GamesApplication.class, args);
 	}
 
