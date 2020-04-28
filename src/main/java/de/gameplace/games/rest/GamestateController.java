6764d29eff1b0425e3c222aae58934020d49b814
Jules <jul.sandrock@gmail.com>
2020-04-28 14:51:00 +0200

    Change temporary RestController for game infos

--- src/main/java/de/gameplace/games/rest/GamestateController.java
+++ src/main/java/de/gameplace/games/rest/GamestateController.java
@@ -1,34 +1,28 @@
 package de.gameplace.games.rest;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import de.gameplace.games.model.Game;
-import de.gameplace.games.model.GameRound;
-import de.gameplace.games.model.Player;
+import de.gameplace.games.exceptions.GameException;
+import de.gameplace.games.service.GameManager;
+import de.gameplace.games.service.GameManagerService;
 
 @RestController
 public class GamestateController {
 
     @Autowired
-    Game game;
+    GameManagerService gameManagerService;
 
-    @GetMapping("/game")
-    public Game getGame() {
-        return game;
+    @GetMapping("/all")
+    public GameManagerService getAll() {
+        return gameManagerService;
     }
 
-    @GetMapping("/gameround")
-    public GameRound getGameRound() {
-        return game.getGameRound();
-    }
-
-    @GetMapping("/playersintern")
-    public List<Player> getPlayersIntern() {
-        return game.getPlayers();
+    @GetMapping("/getGame")
+    public GameManager getGame(@RequestParam(value = "gameId", defaultValue = "") String gameId) throws GameException {
+        return gameManagerService.getGameManager(gameId);
     }
 
 }
