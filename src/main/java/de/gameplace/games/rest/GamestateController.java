0000afafe947b1e9ec513f3a29a9e22cc225f1ab
Jules <jul.sandrock@gmail.com>
2020-04-28 18:18:31 +0200

    add helper methods to show current state

--- src/main/java/de/gameplace/games/rest/GamestateController.java
+++ src/main/java/de/gameplace/games/rest/GamestateController.java
@@ -1,5 +1,7 @@
 package de.gameplace.games.rest;
 
+import java.util.Collection;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,6 +25,11 @@ public class GamestateController {
     @GetMapping("/getGame")
     public GameManager getGame(@RequestParam(value = "gameId", defaultValue = "") String gameId) throws GameException {
         return gameManagerService.getGameManager(gameId);
+    }
+
+    @GetMapping("/getGameManagers")
+    public Collection<GameManager> getGameManagers() throws GameException {
+        return gameManagerService.getGameManagers();
     }
 
 }
